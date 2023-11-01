acf51be4f641ab57cb768b9d9648510c8bfbe4c3
Manikandan Anbalagan <manikandana121290@gmail.com>
2023-11-01 22:04:10 +0530

    task

--- Async-Day2-Additional/fetch.js
+++ Async-Day2-Additional/fetch.js
@@ -1,5 +1,28 @@
-fetch("https://restcountries.com/v3.1/all")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  });
+// fetch("https://restcountries.com/v3.1/all")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+document.getElementById("weatherInfo").addEventListener("click", () => {
+  const country = "France";
+  const api = "19a8470a463433487b03f8f82158f8e9";
+
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const capital = data[0].capital[0];
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api}`
+      );
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById(
+        "details"
+      ).innerHTML = `Weather is ${data.name}: ${data.weather[0].main}, Temp: ${data.main.temp}`;
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+});
