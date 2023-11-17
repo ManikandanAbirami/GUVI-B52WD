61831547dfad824b440f631ca6d439d002ff5ba9
Manikandan Anbalagan <manikandana121290@gmail.com>
2023-11-17 21:24:07 +0530

    RD3

--- React-Day3/lists-react/src/App.jsx
+++ React-Day3/lists-react/src/App.jsx
@@ -35,6 +35,12 @@ function App() {
             </li>
         })}
       </ul>
+      <div className="card" id='main'>
+        <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
   )
 }
